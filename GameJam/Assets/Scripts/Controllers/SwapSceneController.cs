fd686bad8955f23c1bee73397b5515b60acbf2c1
ivan.lashin <iw.lasgin2011@gmail.com>
2019-01-26 12:36:55 +0300

    add swap scene logic

--- GameJam/Assets/Scripts/Controllers/SwapSceneController.cs
+++ GameJam/Assets/Scripts/Controllers/SwapSceneController.cs
@@ -3,5 +3,10 @@ using UnityEngine.SceneManagement;
 
 public static class SwapSceneController
 {
-
+    public static void SwapScene() {
+        var currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "Preloader") {
+            SceneManager.LoadScene("Worm Plus Meat Scene");
+        }
+    }
 }
